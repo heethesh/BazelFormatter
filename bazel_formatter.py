e3ce3feb0298ecda8e407625cce3bc3166f79509
Heethesh Vhavle <heethesh@aeva.ai>
2022-09-20 20:18:32 -0700

    update path

--- bazel_formatter.py
+++ bazel_formatter.py
@@ -8,7 +8,8 @@ class BazelFormatterCommand(sublime_plugin.TextCommand):
 
     def run(self, edit):
         filename = self.view.file_name()
-        ret = subprocess.run(['/home/heethesh/go/bin/buildifier', filename], capture_output=True)
+        # TODO: Better handle installation directory of buildifier and go/bin PATH.
+        ret = subprocess.run(['buildifier', filename], capture_output=True)
         if ret.returncode != 0:
             self.view.window().status_message('Failed to run buildifier on ' + filename +
                                               '\nError: ' + ret.stdout)
